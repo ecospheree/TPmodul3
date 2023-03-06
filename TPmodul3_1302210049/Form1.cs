cf5b15a30b27697f62eb7270f7b3c3adc2e8a51e
ecospheree <121998521+ecospheree@users.noreply.github.com>
2023-03-06 03:18:12 -0800

    Tambahan fitur GUI

--- TPmodul3_1302210049/Form1.cs
+++ TPmodul3_1302210049/Form1.cs
@@ -1,10 +1,26 @@
-namespace TPmodul3_1302210049
+﻿namespace TPmodul3_1302210049
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = textBox1.Text;
+            label1.Text = " 안녕하세요 "+ input;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
